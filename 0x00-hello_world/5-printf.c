85a079f6729501bbf7c6dc332618a39c5a0baefe
GreenCyberNinja <1824@holbertonschool.com>
2020-06-11 01:05:14 +0000

    printing in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - function that expects no arguments
+*
+* printf - prints a formatted output
+*
+* Return: - void
+*/
+
+int main(void)
+{
+	printf(" with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
