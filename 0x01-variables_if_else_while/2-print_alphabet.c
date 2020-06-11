0ad28a27ec265fc60f6b79b743e97e4d9619bbd7
GreenCyberNinja <1824@holbertonschool.com>
2020-06-11 23:43:39 +0000

    alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print the alphabet with putchar
+*
+* Return:void
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+	return (0);
+}
