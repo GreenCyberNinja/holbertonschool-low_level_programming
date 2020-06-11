3e553218c3503def9a0aaa3007cb9b44ff26b16f
GreenCyberNinja <1824@holbertonschool.com>
2020-06-11 00:07:09 +0000

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - function that expects no arguments
+*
+* puts - prints sting to standard output
+*
+* Return: write string to standard output
+*/
+
+int main(void)
+
+{
+	puts("\"Programing is like building a multilingual puzzle");
+
+	return (0);
+}
