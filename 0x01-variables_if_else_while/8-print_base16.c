3385cedeb259b198304ecf883955806f215165fe
GreenCyberNinja <1824@holbertonschool.com>
2020-06-12 04:39:29 +0000

    hexadecimal range

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - putchar hexadecimal range
+*
+* Return: void
+*/
+
+int main(void)
+{
+	char num;
+
+	char CH;
+
+	for (num = '0' ; num <= '9' ; num++)
+{
+	putchar(num);
+}
+	for (CH = 'a' ; CH <= 'f' ; CH++)
+{
+	putchar(CH);
+}
+	putchar('\n');
+}
