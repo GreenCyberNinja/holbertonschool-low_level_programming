f2dd7b38b6100cfecd76994277702e68ec3b5a46
GreenCyberNinja <1824@holbertonschool.com>
2020-06-12 03:29:58 +0000

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - alphabet in reverse
+*
+* Return: void
+*/
+
+int main(void)
+{
+	char z;
+
+	for (z = 'z'; z <= 'a'; z++)
+{
+	putchar(z);
+}
+	putchar('\n');
+	return (0);
+}
