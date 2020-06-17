0900dc3bcdd38ecf113cb695bb2855ef286c569e
GreenCyberNinja <1824@holbertonschool.com>
2020-06-17 05:25:35 +0000

    is it lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* _islower - returns 1 if lowercase
+*
+* @c: input for program
+*
+* Return: int r
+*/
+int _islower(int c)
+{
+	int r;
+
+	if (c >= 97 || c <= 122)
+{
+	r = 1;
+}
+	if (c >= 65 && c <= 90)
+{
+	r = 0;
+}
+	return (r);
+}
