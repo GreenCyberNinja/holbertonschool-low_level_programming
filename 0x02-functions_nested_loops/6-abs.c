2af6a78257819aad15f8a8fec4a2d5a2563bd3c4
GreenCyberNinja <1824@holbertonschool.com>
2020-06-18 01:02:00 +0000

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
+* _abs - returns absolute value
 *
+* @a: input
 *
-*
-*
+* Return: return a
 */
 int _abs(int a)
 {
@@ -12,11 +13,11 @@ int _abs(int a)
 {
 	a = (a * -1);
 
-	return(a);
-}	 
+	return (a);
+}
 	else if (a >= 0)
 {
 	a = (a * 1);
 }
-	return(a);
+	return (a);
 }
