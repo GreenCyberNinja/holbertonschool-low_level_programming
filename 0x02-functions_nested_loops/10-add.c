ffaecf607fb4eb7dbd50632a17a5705bcc5a8d84
GreenCyberNinja <1824@holbertonschool.com>
2020-06-18 06:30:50 +0000

    simple addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* add - adds two int together
+*
+* @a: input 1
+*
+* @b: input 2
+*
+* Return: result
+*/
+
+int add(int a, int b)
+{
+	int r;
+
+	r = (a + b);
+
+	return(r);
+}
