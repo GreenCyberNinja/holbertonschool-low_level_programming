8d17ff4b3ed7ea57fef99b03805d3a00aa0c6210
GreenCyberNinja <1824@holbertonschool.com>
2020-06-18 21:04:44 +0000

    multiplication table of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
 
 /**
+* times_table - multiplication table of 9
 *
-*
-*
-*
-*
+* Return: void
 */
 void times_table(void)
 {
@@ -13,17 +11,28 @@ void times_table(void)
 	int co;
 	int r;
 
-	for (li = '0' ; li <= '9' ; li++)
+	for (li = 0 ; li <= 9 ; li++)
 {
 
-	for (co = '0' ; co <= '9' ; co++)
+	for (co = 0 ; co <= 9 ; co++)
 
 {
 	r = (li * co);
-
-	_putchar(r + '0');
+	if (r <= 9)
+{
+	_putchar(' ');
+	_putchar(r % 10 + '0');
+}
+	else
+{
+	_putchar(r / 10 + '0');
+	_putchar(r % 10 + '0');
+}
+	if (co < 9)
+{
 	_putchar(',');
 	_putchar(' ');
+}
 }
 	_putchar('\n');
 }
