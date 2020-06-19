cc5efdebc6f46c419dfe7ced6673a13ae1d0c79a
GreenCyberNinja <1824@holbertonschool.com>
2020-06-19 06:57:19 +0000

    prints numbers 0-9 without 2,3

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* print_most_numbers - print the alphabet with putchar
+*
+* Return:void
+*/
+int print_most_numbers(void)
+{
+	char ch;
+
+	for (ch = 48 ; ch <= 57 ; ch++)
+{
+	if (ch != 50 && ch != 52)
+
+	putchar(ch);
+}
+	putchar('\n');
+
+	return (0);
+}
