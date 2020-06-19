23045ac0dc8ac8344507551c0b2e8916a0460512
GreenCyberNinja <1824@holbertonschool.com>
2020-06-19 02:00:02 +0000

    holberton header file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,46 @@
+/**
+* checks for uppercase
+*/
+int _isupper(int c);
+/**
+* checks for digit(0-9)
+*/
+int _isdigit(int c);
+/**
+* multiplies two integers
+*/
+int mul(int a, int b);
+/**
+* prints 0-9 followed by new line
+*/
+void print_numbers(void);
+/**
+* prints 0-9 followed by new line
+*/
+void print_most_numbers(void);
+/**
+*
+*/
+void more_numbers(void);
+/**
+*
+*/
+void print_line(int n);
+/**
+*
+*
+*/
+void print_diagonal(int n);
+/**
+*
+*
+*/
+void print_square(int size);
+/**
+*
+*/
+void print_triangle(int size);
+/**
+*
+*/
+int _putchar(char c);
