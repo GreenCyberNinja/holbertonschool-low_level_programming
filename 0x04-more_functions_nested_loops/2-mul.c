733d84011351e2f5b840f01cb7da3c9715d3a0ab
GreenCyberNinja <1824@holbertonschool.com>
2020-06-19 04:59:12 +0000

    multiplies two interger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* mul - multiplies two intergers
+*
+* @a: input 1
+*
+* @b: input 2
+*
+* Return: result
+*/
+int mul(int a, int b)
+{
+	int r;
+
+	r = (a * b);
+
+	return (r);
+}
