571857178079ccfb92fc75a1a8050c49d5a4b002
GreenCyberNinja <1824@holbertonschool.com>
2020-06-19 04:39:53 +0000

    checks if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* _isdigit - checks to see if digit
+*
+* @c: input
+*
+* Return : result
+*/
+int _isdigit(int c)
+{
+	int r;
+
+	if (c >= 48 && c <= 57)
+{
+	r = 1;
+}
+	else if (c >= 48 && c <= 57)
+{
+	r = 1;
+}
+	else
+{
+	r = 0;
+}
+	return (r);
+}
