0b4876897b6d18391b30a0ac1ab5fd6c5142e486
GreenCyberNinja <1824@holbertonschool.com>
2020-06-20 02:52:50 +0000

    prints a line of imput length

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* print_line - prints a line thats the distance of n
+*
+* @n: - input
+*/
+void print_line(int n)
+{
+	int s;
+
+	if (n > 0)
+{
+	for (s = 0 ; s <= n ; s++)
+{
+	_putchar('_');
+}
+}
+	_putchar('\n');
+}
