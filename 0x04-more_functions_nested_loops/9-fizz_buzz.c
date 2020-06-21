49757a17493594f8bbfb701f6e12a2bfc6346bc1
GreenCyberNinja <1824@holbertonschool.com>
2020-06-21 01:47:00 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+*
+*
+*
+*
+*/
+
+int main(void)
+{
+	int t;
+
+	for (t = 1 ; t <= 100 ; t++)
+{
+	if ((t % 3) == 0 && (t % 5) != 0)
+{
+	printf("Fizz");
+}
+	if ((t % 3) != 0 && (t % 5) == 0)
+{
+	printf("Buzz");
+}
+	if ((t % 3) == 0 && (t % 5) == 0)
+{
+	printf("FizzBuzz");
+}
+	if ((t % 3) != 0 && (t % 5) != 0)
+{
+	printf("%d", t);
+}
+	putchar(' ');
+}
+	putchar('\n');
+	return (0);
+}
