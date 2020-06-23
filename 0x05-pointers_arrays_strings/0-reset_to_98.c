d9e72dbb9f14a605c3176b213de7dca754e29dd1
GreenCyberNinja <1824@holbertonschool.com>
2020-06-23 03:53:55 +0000

    project 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - resets pointer n to 98
+*
+* @n: input pointer
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*
+*
+*
+*
+*/
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0 ; *s != '\0' ; s++)
+{
+	n++;
+}
+	return (n);
+}
