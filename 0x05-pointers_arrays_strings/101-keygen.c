3ce858455b9b70feb002e1b79947a8527828c222
GreenCyberNinja <1824@holbertonschool.com>
2020-06-24 20:40:44 +0000

    lock stock and key

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlb.h>
+#include <time.h>
+/**
+* main - cycles through all password combos at once
+*
+* Return: void
+*/
+
+int main (void)
+{
+	char d[63] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	char password[100];
+	int x;
+	int y;
+	int sum;
+
+	srand (time(NULL));
+	sum = 0;
+	x = 0;
+
+	while (sum <(27772 - 122))
+
+{
+	y = rand() % 62;
+	password[x] = d[y];
+	sum = sum + password[x];
+	x++;
+
+}
+	y = 2772 - sum;
+	password[x] = y;
+	x++;
+	password[x] = '\0';
+	printf("%s", password)
+	
+	return (0);
+}	
