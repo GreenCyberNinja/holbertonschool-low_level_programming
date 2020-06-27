0eca4f12618ee2b925c824607b50604227820238
GreenCyberNinja <1824@holbertonschool.com>
2020-06-27 23:18:21 +0000

    compares strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
-*
-*
-*
-*
+* _strcmp - compares string
+* @s1: input string 1
+* @s2: input string 2
+* Return: returns zero, negative or positive
 */
 int _strcmp(char *s1, char *s2)
 {
-	for( ; *s1 == *s2 ; s1++, s2++)
+	for ( ; *s1 == *s2 ; s1++, s2++)
 {
 	if (*s1 == '\0')
 {
 	return (0);
 }
 }
-	return *s1 - *s2;
+	return (*s1 - *s2);
 }
