572c56f80992790eae12845165a534990a7a0d88
GreenCyberNinja <1824@holbertonschool.com>
2020-07-06 02:14:40 +0000

    prints the name of the exuctable

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - prints the name of the file
+* @argc: argument count
+* @argv: string arayment
+*
+* Return: void
+*/
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
