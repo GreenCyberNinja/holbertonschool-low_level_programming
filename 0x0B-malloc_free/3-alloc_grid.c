d568d8de08e4d1a11a2d1b7ffca4b2dab9125824
GreenCyberNinja <1824@holbertonschool.com>
2020-07-08 05:39:31 +0000

    allocates for 2d array grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* alloc_grid - alocates memory for a 2 dimensional array
+*
+* @height: height of the array
+* @width: width of the array
+*
+* Return: either *a or NULL
+*/
+int **alloc_grid(int width, int height)
+{
+	int col;
+	int row;
+	int **a;
+
+	if (width <= 0 || height <= 0)
+	return (NULL);
+
+	a = (int **) malloc(sizeof(int *) * height);
+	if (a == NULL)
+	{
+		free(a);
+	return (NULL);
+	}
+	for (col = 0; col < height; col++)
+	{
+		a[col] = (int *)malloc(sizeof(int) * width);
+
+		if (a[col] == NULL)
+		{
+			for (row = 0; row <= col; row++)
+			{
+				free(a[row]);
+			{
+				free(a);
+				return (NULL);
+		}
+		for (row = 0; row < width; row++)
+		{
+			a[col][row] = 0;
+		}
+	}
+	return (a);
+}
