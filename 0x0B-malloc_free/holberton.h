eafc1528cb8a14edbe5fc68b7695a2449c1a98d2
GreenCyberNinja <1824@holbertonschool.com>
2020-07-08 00:49:19 +0000

    project header file

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,31 @@
+#ifndef HOLBERTON_H
+#define HOBERTON_H
+
+#include <stdio.h>
+
+#endif
+int _putchar(char c);
+/**
+*
+*/
+char *create_array(unsigned int size, char c);
+/**
+*
+*/
+char *_strdup(char *str);
+/**
+*
+*/
+char *str_concat(char *s1, char *s2);
+/**
+*
+*/
+int **alloc_grid(int width, int height);
+/**
+*
+*/
+void free_grid(int **grid, int height);
+/**
+*
+*/
+char *argstostr(int ac, char **av);
