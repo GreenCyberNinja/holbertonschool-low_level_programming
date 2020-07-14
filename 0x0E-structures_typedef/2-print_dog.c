89b09306ac190068ca230913993106a2982e34be
GreenCyberNinja <1824@holbertonschool.com>
2020-07-14 03:47:56 +0000

    betty fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "dog.h"
 /**
-*
-*
-*
+* print_dog - prints dog struct
+* @d: struct
+* Return: null
 *
 */
 void print_dog(struct dog *d)
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		printf("owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 	}
 
 }
