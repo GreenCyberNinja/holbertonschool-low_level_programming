84f74ddd4d218fad7506f862fbbb02ade8746d83
GreenCyberNinja <1824@holbertonschool.com>
2020-07-16 00:57:49 +0000

    first push

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "calc.h"
+/**
+* op_add - operation addition
+* @a: input 1
+* @b: input 2
+* Return: addition
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - operation subtraction
+* @a: input 1
+* @b:input 2
+* Return: subtraction
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - operation multiply
+* @a: input 1
+* @b: input 2
+* Return: multiplication
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - division
+* @a: input1
+* @b: input2
+* Return: dividend
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - operation division remainder
+* @a: input 1
+* @b: input 2
+* Return: division remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
