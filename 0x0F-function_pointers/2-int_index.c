bb8411fcc3c153be3611880283dc68d28723a120
GreenCyberNinja <1824@holbertonschool.com>
2020-07-16 02:31:05 +0000

    fixed edge case and betty

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,18 +1,19 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
+* int_index - finds a number in an array
 *
+* @array: is what it is
+* @size: size of array
+* @cmp: compare function
 *
-*
-*
-*
-*
+* Return: -1 or -ind
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int ind;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 	return (-1);
 
 	for (ind = 0; ind <= size; ind++)
@@ -22,5 +23,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[ind]) != 0)
 		return (ind);
 	}
-	return (-1);
+	return (0);
 }
