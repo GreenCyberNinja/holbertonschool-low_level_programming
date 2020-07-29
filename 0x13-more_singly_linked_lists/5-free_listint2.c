166aab2cfddbe03b76264be295c913b824115737
GreenCyberNinja <1824@holbertonschool.com>
2020-07-29 03:57:32 +0000

    frees and sets to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* free_listint2 - frees and sets head to null
+* @head: list to be freed and set to null
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	head = NULL;
+}
