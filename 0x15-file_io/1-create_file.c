160f51432fb6dc12dbb5dc87e9e8ff1cb06753a9
GreenCyberNinja <1824@holbertonschool.com>
2020-08-05 01:57:20 +0000

    betty fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	size_t ind;
-	int check = open(filename, O_CREAT|O_WRONLY|O_TRUNC, 0600);
+	int check = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (check == -1)
 		return (-1);
