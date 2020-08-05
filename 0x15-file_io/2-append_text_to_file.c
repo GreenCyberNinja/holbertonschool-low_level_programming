2ccab768239edcf0bf56c730b729303cb374bbb3
GreenCyberNinja <1824@holbertonschool.com>
2020-08-05 03:01:14 +0000

    added null case

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,12 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int ind;
-	int check = open(filename, O_APPEND | O_WRONLY);
+	int check;
+
+	if (filename == NULL)
+		return (-1);
+
+	check = open(filename, O_APPEND | O_WRONLY);
 
 	if (check == -1)
 		return (-1);
