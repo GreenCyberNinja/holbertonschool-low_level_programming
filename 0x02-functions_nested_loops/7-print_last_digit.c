50c5de077b73744f70114abcc4cea5e9b6b96fcb
GreenCyberNinja <1824@holbertonschool.com>
2020-08-20 21:19:16 +0000

    updated to handle int_min

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,18 +9,13 @@
 */
 int print_last_digit(int a)
 {
-	if (a < 0)
-{
-	a = (a * (-1));
-	_putchar('0' + (a % 10));
-	return (a % 10);
-}
+	int b;
 
-	else
-{
+	b = a % 10;
+	if (b < 0)
+		b = b * -1;
 
-	_putchar('0' + (a % 10));
-	return (a % 10);
-}
+	_putchar('0' + b);
+
 	return (a);
 }
