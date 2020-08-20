5f8db83d0c46a2242fca9393147ae9ea965bfc17
GreenCyberNinja <1824@holbertonschool.com>
2020-08-20 21:43:25 +0000

    fixed my fixes issue

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,11 @@
 * Return: last digit
 */
 int print_last_digit(int a)
-{
-	int b;
+{ 
+	int b = (a % 10);
 
-	b = a % 10;
 	if (b < 0)
-		b = b * -1;
-
-	_putchar('0' + b);
-
-	return (a);
+		b *= -1;
+	_putchar(b + '0');
+	return (b);
 }
