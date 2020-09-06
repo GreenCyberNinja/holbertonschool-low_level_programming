230a83a083054a85e551b2e80c21feae101e8ebf
GreenCyberNinja <1824@holbertonschool.com>
2020-09-06 20:03:13 +0000

    betty compliant and additional edge case

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-* add_dnoteint - adds a node at the beginning of list
+* add_dnodeint - adds a node at the beginning of list
 * @head: list to add node
 * @n: data to insert into node
 * Return: returns new address
@@ -12,7 +12,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
+	if n == NULL
+		return (NULL);
 	new->n = n;
 	new->next = *head;
 	*head = new;
