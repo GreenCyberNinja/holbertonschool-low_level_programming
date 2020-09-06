075f940aec08ba8980d11de6387ea2ec3661d107
GreenCyberNinja <1824@holbertonschool.com>
2020-09-06 16:24:40 +0000

    prints list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* print_dlistint - prints a lists contents
+* @h: list to print
+* Return: returns elements printed
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+
+	for (i = 0 ; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
