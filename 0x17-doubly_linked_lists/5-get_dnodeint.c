4f1c4d9dd53833954d21aa7146c0fca7cffe2f04
GreenCyberNinja <1824@holbertonschool.com>
2020-09-07 05:05:36 +0000

    final check correction

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,7 +9,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-
+	if (head == NULL)
+		return (NULL);
 	if (head->prev != NULL)
 		while (head->prev == NULL)
 			head = head->prev;
