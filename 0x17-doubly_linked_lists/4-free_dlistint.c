8de1ea9f4d47a58e9df17d364f799feebb35034a
GreenCyberNinja <1824@holbertonschool.com>
2020-09-07 04:56:14 +0000

    fixed missing check

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,8 +5,13 @@
 */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *tmp;
 	if (head == NULL)
 		return;
+
+	if (head->prev != NULL)
+		while(head->prev != NULL)
+			head = head-> NULL;
 
 	while (head != NULL)
 	{
