770201b68bb2b0ed6dbd9772d38b50bf1bdd9f23
GreenCyberNinja <1824@holbertonschool.com>
2020-09-07 01:10:11 +0000

    fixed NULL issue

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -22,6 +22,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	tmp->next = new;
 	new->n = n;
 	new->next = NULL;
-	new->prev = *head;
+	if (*head != NULL)
+		new->prev = *head;
 	return (new);
 }
