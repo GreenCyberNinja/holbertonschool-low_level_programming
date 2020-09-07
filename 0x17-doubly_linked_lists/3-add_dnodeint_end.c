307e4952494f3650e56705139046d89541866d6d
GreenCyberNinja <1824@holbertonschool.com>
2020-09-07 01:58:39 +0000

    fixed prev pointer issue

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,6 +10,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new, *tmp;
 
 	new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
 		return (NULL);
 	if (*head == NULL)
@@ -22,9 +23,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	tmp->next = new;
 	new->n = n;
 	new->next = NULL;
-	if (*head != NULL)
-		new->prev = *head;
-	else
-		new->prev = NULL;
+	new->prev = tmp;
 	return (new);
 }
