c8339b1e9b959d8b1efdec9fc5782d2044ce65ed
GreenCyberNinja <1824@holbertonschool.com>
2020-09-07 04:03:23 +0000

    frees the list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* free_dlistint - frees the list
+* @head: list to be free
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head->prev != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
