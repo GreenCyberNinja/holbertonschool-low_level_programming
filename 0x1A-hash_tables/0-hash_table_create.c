64e768be214d0def975657cd34aa20b12e6ba9c1
GreenCyberNinja <1824@holbertonschool.com>
2020-10-08 19:11:47 +0000

    betty compliant

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,19 +1,26 @@
 #include "hash_tables.h"
 /**
-*
-*
-*
-*
-*
-*
+* hash_table_create - creates hash table
+* @size: size of table
+* Return: new table or NULL
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i;
 	hash_table_t *newt = malloc(sizeof(hash_table_t));
 
+	if (newt == NULL)
+		return (NULL);
+
 	newt->size = size;
 	newt->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (newt->array == NULL)
+	{
+		free(newt);
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 		newt->array[i] = NULL;
 	return (newt);
